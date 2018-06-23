f2b756d181e6e23064013680c575668c1d115850
Joseph Sirianni <joe.sirianni@bluemedora.com>
2018-06-23 13:50:39 -0400

    setup metadata and use a real license

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,10 @@
 name 'consul'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures consul'
-long_description 'Installs/Configures consul'
+maintainer 'Joseph Sirianni'
+maintainer_email 'joseph.sirianni88@gmail.com'
+license 'BSD 2-Clause License'
+description 'Installs/Configures a Consul cluster'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/consul/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/consul'
+issues_url 'https://github.com/jsirianni/cookbook-consul/issues'
+source_url 'https://github.com/jsirianni/cookbook-consul'
