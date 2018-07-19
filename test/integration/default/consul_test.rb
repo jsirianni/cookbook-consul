f8353e0cb761ca80f5ac84a4e9bee196c18bc52f
joe sirianni <joe.sirianni@bluemedora.com>
2018-07-18 21:58:06 -0400

    test with ip and port

--- test/integration/default/consul_test.rb
+++ test/integration/default/consul_test.rb
@@ -1,3 +1,6 @@
+# Read node attributes
+node = json("/tmp/kitchen/dna.json").params
+
 # Archive should exist
 describe file('/tmp/consul.zip') do
     its('mode') { should cmp '0755' }
@@ -13,17 +16,17 @@ describe file('/usr/local/bin/consul') do
 end
 
 # Verify version 1.1.0
-describe command('sudo consul --version | grep Consul | cut -c 9-13') do
+describe command("sudo consul --version -http-addr #{node['consul']['conf']['bind_addr']}:8500 | grep Consul | cut -c 9-13") do
    its('stdout') { should match (/1.2.1/) }
 end
 
 # Check member status
-describe command('sudo consul members') do
+describe command("sudo consul members -http-addr #{node['consul']['conf']['bind_addr']}:8500") do
     its('exit_status') { should eq 0 }
 end
 
 # default-consul-0 should be the leader
-describe command('sudo consul operator raft list-peers | grep default-consul-0 | cut -c 74-79') do
+describe command("sudo consul operator raft list-peers -http-addr #{node['consul']['conf']['bind_addr']}:8500 | grep default-consul-0 | cut -c 74-79") do
     its('exit_status') { should eq 0 }
     its('stdout') { should match (/leader/) }
 end
